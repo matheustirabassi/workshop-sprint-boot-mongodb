e504c16cf0f2722f138a0c72e9899cbae0bec54c
Matheus Tirabassi <tirabassi.matheus@aluno.ifsp.edu.br>
2020-11-29 18:21:52 -0300

    Operacao de instanciacao da base de dados

---
+++ src/main/java/com/matheustirabassi/workshopmongo/config/Instantiation.java
@@ -0,0 +1,27 @@
+package com.matheustirabassi.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.matheustirabassi.workshopmongo.domain.User;
+import com.matheustirabassi.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		userRepository.deleteAll();
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+
+}
