f3dbee0a2889d4d56b9df6efd632791e31cb2ad1
Matheus Tirabassi <tirabassi.matheus@aluno.ifsp.edu.br>
2020-12-05 17:09:03 -0300

    Inserção de usuário com POST

--- src/main/java/com/matheustirabassi/workshopmongo/services/UserService.java
+++ src/main/java/com/matheustirabassi/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.matheustirabassi.workshopmongo.domain.User;
+import com.matheustirabassi.workshopmongo.dto.UserDTO;
 import com.matheustirabassi.workshopmongo.repository.UserRepository;
 import com.matheustirabassi.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -22,5 +23,12 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not founded"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(),objDTO.getName(),objDTO.getEmail());
 	}
 }
