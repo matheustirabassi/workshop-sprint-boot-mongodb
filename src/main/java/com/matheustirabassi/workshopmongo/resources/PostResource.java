9d645fb4834cb01f12a3a4038a9644f7f3d4ae13
Matheus Tirabassi <tirabassi.matheus@aluno.ifsp.edu.br>
2020-12-11 18:48:01 -0300

    consulta com vários critérios

--- src/main/java/com/matheustirabassi/workshopmongo/resources/PostResource.java
+++ src/main/java/com/matheustirabassi/workshopmongo/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.matheustirabassi.workshopmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,10 +27,22 @@ public class PostResource {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+
 	@RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String txt) {
 		txt = URL.decodeParam(txt);
 		List<Post> list = service.findByTitle(txt);
+		return ResponseEntity.ok().body(list);
+	}
+
+	@RequestMapping(value = "/fullsearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> fullSearch(@RequestParam(value = "text", defaultValue = "") String text,
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = service.fullSearch(text, min, max);
 		return ResponseEntity.ok().body(list);
 	}
 }
